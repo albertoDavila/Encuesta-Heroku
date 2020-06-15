7a119b75d9197f8bf60e1b049859837ee648da5d
albertoDavila <48994619+albertoDavila@users.noreply.github.com>
2020-06-15 12:46:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use( morgan( "dev" ) );
 // 		});
 // });
 
-app.get( "/api/EncuestasAn2", ( req, res, next ) => {
+app.get( "/api/EncuestasE", ( req, res, next ) => {
 	VPList.get()
 		.then( vicepresidencia => {
 			return res.status( 200 ).json( vicepresidencia );

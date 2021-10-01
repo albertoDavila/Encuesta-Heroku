f8ab13e906e35786556f2d5d702276f24f4a995e
albertoDavila <48994619+albertoDavila@users.noreply.github.com>
2021-10-01 09:56:54 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -38,6 +38,40 @@ function init(){
 		// alert(document.getElementById("tiempoini").value);
 		
 	});
+	
+		document.getElementById("post5").addEventListener("click", function(e) {
+		var id  = document.getElementById("ID").value;
+		var tFin = document.getElementById("prediccion").value;
+
+		let url2 = '/api/prediccion'
+		let settings2 = {
+			method: 'POST',
+			body: JSON.stringify({
+				id: id,
+				tiempoFinal: parseInt(tFin)
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		}
+
+		fetch(url2, settings2)
+		.then( response => {
+			if ( response.ok ){
+				return response.json();
+			}
+
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {
+
+			console.log(responseJSON);
+		})
+		.catch( err => {
+			console.log( err );
+		})
+		// alert(document.getElementById("tiempoini").value);
+	});
 
 	document.getElementById("post2").addEventListener("click", function(e) {
 		var id  = document.getElementById("Name2").value;

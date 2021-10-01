fbbff00bf059ea777b5c3ab6a2c7e0a6a8a7ec52
albertoDavila <48994619+albertoDavila@users.noreply.github.com>
2021-10-01 10:11:34 -0500

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,51 +4,18 @@ function init(){
 		method: 'GET'
 	}
 
-	document.getElementById("post").addEventListener("click", function(e) {
-		var id  = document.getElementById("nombre").value;
-		var tIni = document.getElementById("precio").value;
 
-		let url2 = '/api/EjerecioAn'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				tiempoInicio: parseInt(tIni)
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
 
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-		
-	});
-	
-		document.getElementById("post5").addEventListener("click", function(e) {
+	document.getElementById("post5").addEventListener("click", function(e) {
 		var id  = document.getElementById("ID").value;
-		var tFin = document.getElementById("prediccion").value;
+		var pred = document.getElementById("prediccion").value;
 
 		let url2 = '/api/prediccion'
 		let settings2 = {
 			method: 'POST',
 			body: JSON.stringify({
-				id: id,
-				tiempoFinal: parseInt(tFin)
+				ID: id,
+				prediccion: pred
 			}),
 			headers: {
 				'Content-Type': 'application/json'
@@ -73,178 +40,7 @@ function init(){
 		// alert(document.getElementById("tiempoini").value);
 	});
 
-	document.getElementById("post2").addEventListener("click", function(e) {
-		var id  = document.getElementById("Name2").value;
-		var tFin = document.getElementById("tiempofinal").value;
 
-		let url2 = '/api/EjercicioAn2'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				tiempoFinal: parseInt(tFin)
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
-
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-	});
-
-	document.getElementById("post3").addEventListener("click", function(e) {
-		var id  = document.getElementById("Name3").value;
-		var nIvel = document.getElementById("Nivel").value;
-
-		let url2 = '/api/EjercicioAn3'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				nivel: parseInt(nIvel)
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
-
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-	});
-
-	document.getElementById("post4").addEventListener("click", function(e) {
-		var id  = document.getElementById("Name4").value;
-		var tAcum = document.getElementById("tAcum").value;
-
-		let url2 = '/api/EjercicioAn4'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				tiempoAcumulado: parseInt(tAcum)
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
-
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-	});
-
-	document.getElementById("post5").addEventListener("click", function(e) {
-		var id  = document.getElementById("Name5").value;
-		var dAcum = document.getElementById("dAcum").value;
-
-		let url2 = '/api/EjercicioAn5'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				diasAcum: parseInt(dAcum)
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
-
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-	});
-
-	document.getElementById("post6").addEventListener("click", function(e) {
-		var id  = document.getElementById("Name6").value;
-		var dExp = document.getElementById("dExp").value;
-
-
-		console.log(typeof(dExp))
-
-		let url2 = '/api/EjercicioAn6'
-		let settings2 = {
-			method: 'POST',
-			body: JSON.stringify({
-				id: id,
-				exp: dExp
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		}
-
-		fetch(url2, settings2)
-		.then( response => {
-			if ( response.ok ){
-				return response.json();
-			}
-
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {
-
-			console.log(responseJSON);
-		})
-		.catch( err => {
-			console.log( err );
-		})
-		// alert(document.getElementById("tiempoini").value);
-	});
 
 
 	fetch(url, settings)
@@ -259,8 +55,7 @@ function init(){
 
 			for ( let i = 0; i < responseJSON.length; i ++ ){
 				$('.listOfVPs').append(`<li>
-				${responseJSON[i].nombre} - Tiene tiempo de Inicio: ${responseJSON[i].tiempoInicio} 
-										- Tiene experiencia: ${responseJSON[i].experiencia}</li>`)
+				${responseJSON[i].nombre} - Se le dio como prediccion: ${responseJSON[i].prediccion} </li>`)
 			}
 		})
 		.catch( err => {
